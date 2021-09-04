f105fdcc2a27191d79d08eefb4efc96efbbaf47e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-04 19:34:25 -0300

    ajustar.sh cluster

--- cluster/ajustar.sh
+++ cluster/ajustar.sh
@@ -3,21 +3,6 @@ echo ""
 export ANSIBLE_PYTHON_INTERPRETER=auto_silent
 export ANSIBLE_DEPRECATION_WARNINGS=false
 #sleep 10
-# Aguardando Node1
-export IP=$(terraform output Node_1_ip_externo)
-echo "   Aguardando configurações do $IP: "
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { printf .; sleep 1; } done
-echo "   Conectado ao $IP, verificando ajustes: "
-# Aguardando Node2
-export IP=$(terraform output Node_2_ip_externo)
-echo "   Aguardando configurações do $IP: "
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO2" | grep CONECTADO2 | wc -l) != '1' ]; do { printf .; sleep 1; } done
-echo "   Conectado ao $IP, verificando ajustes: "
-# Aguardando Node3
-export IP=$(terraform output Node_3_ip_externo)
-echo "   Aguardando configurações do $IP: "
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO3" | grep CONECTADO3 | wc -l) != '1' ]; do { printf .; sleep 1; } done
-echo "   Conectado ao $IP, verificando ajustes: "
 
 # configurar hostnames
 terraform output Node_1_ip_externo > hosts &&
--- cluster/iniciar.sh
+++ cluster/iniciar.sh
@@ -1,6 +1,21 @@
 terraform init; terraform plan; terraform apply -auto-approve
 echo ""
 echo " Iniciando configurações: "
+# Aguardando Node1
+export IP=$(terraform output Node_1_ip_externo)
+echo "   Aguardando Node1 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
+# Aguardando Node2
+export IP=$(terraform output Node_2_ip_externo)
+echo "   Aguardando Node2 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO2" | grep CONECTADO2 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
+# Aguardando Node3
+export IP=$(terraform output Node_3_ip_externo)
+echo "   Aguardando Node3 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO3" | grep CONECTADO3 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
 sh ajustar.sh
 #sh config-node1.sh
 #sh config-node2.sh
