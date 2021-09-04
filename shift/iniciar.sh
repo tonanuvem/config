b1ef8fe57c9c4fcb194f151e2b485b2f3a58de72
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-04 19:52:28 -0300

    ajustar iniciar shift

--- shift/iniciar.sh
+++ shift/iniciar.sh
@@ -1,1 +1,37 @@
-terraform init; terraform plan; terraform apply -auto-approve
+echo ""
+echo " Iniciando configurações: "
+MASTER=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_0/) ) { print $1} }')
+NODE1=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_1/) ) { print $1} }')
+NODE2=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_2/) ) { print $1} }')
+NODE3=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_3/) ) { print $1} }')
+echo "IPs configurados :"
+echo "MASTER = $MASTER"
+echo "NODE1 = $NODE1"
+echo "NODE2 = $NODE2"
+echo "NODE3 = $NODE3"
+# Aguardando Master
+export IP=$MASTER
+echo "   Aguardando Node1 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
+# Aguardando Node1
+export IP=$NODE1
+echo "   Aguardando Node1 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
+# Aguardando Node2
+export IP=$NODE2
+echo "   Aguardando Node2 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO2" | grep CONECTADO2 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
+# Aguardando Node3
+export IP=$NODE3
+echo "   Aguardando Node3 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO3" | grep CONECTADO3 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
+sh ajustar.sh
+#sh config-node1.sh
+#sh config-node2.sh
+#sh config-node3.sh
+echo ""
+echo " Cluster iniciado e configurado"
