fd6aad4e37cab36f8adb84364120255d2c97eaef
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-05 00:21:28 -0300

    ajustar shift

--- shift/ajustar.sh
+++ shift/ajustar.sh
@@ -4,10 +4,14 @@ export ANSIBLE_PYTHON_INTERPRETER=auto_silent
 export ANSIBLE_DEPRECATION_WARNINGS=false
 #sleep 10
 
-MASTER=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_0/) ) { print $1} }')
-NODE1=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_1/) ) { print $1} }')
-NODE2=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_2/) ) { print $1} }')
-NODE3=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_3/) ) { print $1} }')
+MASTER=$(terraform output -json ip_externo | jq .[] | jq .[0])
+NODE1=$(terraform output -json ip_externo | jq .[] | jq .[1])
+NODE2=$(terraform output -json ip_externo | jq .[] | jq .[2])
+NODE3=$(terraform output -json ip_externo | jq .[] | jq .[3])
+#MASTER=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_0/) ) { print $1} }')
+#NODE1=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_1/) ) { print $1} }')
+#NODE2=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_2/) ) { print $1} }')
+#NODE3=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_3/) ) { print $1} }')
 
 # configurar hostnames
 echo $MASTER > hosts &&
@@ -20,7 +24,7 @@ echo $NODE3 > hosts &&
 ansible-playbook ~/environment/config/ansible/ansible_hostname.yml --extra-vars "nome=node3" --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
 
 # aplicar configurações
-terraform output Node_1_ip_externo > hosts && terraform output Node_2_ip_externo >> hosts && terraform output Node_3_ip_externo >> hosts
+echo  "$MASTER,$NODE1,$NODE2,$NODE3" > hosts
 ansible-playbook ~/environment/config/ansible/ansible_utils.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem &&
 ansible-playbook ~/environment/config/ansible/ansible_docker.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem &&
 ansible-playbook ~/environment/config/ansible/ansible_k8s.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
