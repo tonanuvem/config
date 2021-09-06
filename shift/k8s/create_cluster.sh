0296d9f5bf28131432687bc305dccc7345cbdbf6
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-06 10:22:02 -0300

    dir

--- shift/k8s/create_cluster.sh
+++ shift/k8s/create_cluster.sh
@@ -1,6 +1,6 @@
 # conectar no master e configurar
 
-DIR = "/home/ec2-user"
+export DIR = "/home/ec2-user"
 # ~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") ) { print } }'
 
 MASTER=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_0/) ) { print $1} }')
