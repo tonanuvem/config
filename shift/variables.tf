3452755b9350922e5afd655f97bb080b6879ee84
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-06 09:41:56 -0300

    var

--- shift/variables.tf
+++ shift/variables.tf
@@ -1,6 +1,6 @@
 variable "instance_type" {
   # default     = "t2.micro"
-  #default     = "t2.medium"
+  # default     = "t2.medium"
   default     = "t2.large" # 4	CPUs e 16 GB
   # default     = "t2.xlarge" # 8	CPUs e 32 GB  
 }
@@ -30,7 +30,8 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# Ubuntu 18.04 LTS (x64)
+# Amazon Linux 2
+# Amazon Linux 2
 variable "aws_amis" {
   default = {
     us-east-1 = "ami-0c2b8ca1dad447f8a"
