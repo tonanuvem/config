c8596f364f7e17bdce27fa1431eb506d9d9db460
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-07 01:27:53 -0300

    rancher

--- shift/rancher_create_cluster.sh
+++ shift/rancher_create_cluster.sh
@@ -22,8 +22,7 @@ ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem e
 DOCKERRUNCMD=$(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'cat DOCKERRUNCMD')
 # Echo command
 echo $DOCKERRUNCMD
-
-TOKEN = $DOCKERRUNCMD
+TOKEN=$DOCKERRUNCMD
 
 #TOKEN=$(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'docker swarm join-token manager | grep docker')
 # Remover espacos
@@ -36,59 +35,19 @@ printf "\n\n"
 
 ### CONFIGURANDO OS NODES utilizando o DOCKER SWARM JOIN:
 
-#echo "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done" >> worker1.sh
-#echo "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done" >> worker2.sh
-#echo "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done" >> worker3.sh
-#echo "sudo hostnamectl set-hostname node1" >> worker1.sh
-#echo "sudo hostnamectl set-hostname node2" >> worker2.sh
-#echo "sudo hostnamectl set-hostname node3" >> worker3.sh
-#echo "sudo $TOKEN" >> worker1.sh
-#echo "sudo $TOKEN" >> worker2.sh
-#echo "sudo $TOKEN" >> worker3.sh
 echo $TOKEN > workers.sh
 
-# Exemplo:
-# docker swarm join --token SWMTKN-1-28amdt0x5r4mbc5092t1w016392emlqv67lyhasph200d6tdhl-41rxupxdsjg9zo00xtdlwon5p 10.1.1.97:2377
 printf "\n\n"
 echo "CONFIGURANDO OS NODES - JOIN:"
 
 for N in $(seq 1 $WORKER_NODES); do
     printf "\n\n"
     NODE=$(terraform output -json ip_externo | jq .[] | jq .[$N] | sed 's/"//g')
-    echo "   CONFIGURANDO NODE $N ($NODE): KUBEADM JOIN"
+    echo "   CONFIGURANDO NODE $N ($NODE): JOIN"
     ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE 'bash -s' < workers.sh
 done
 
-#printf "\n\n"
-#echo "   CONFIGURANDO NODE 1:  JOIN"
-#printf "\n\n"
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE1 'bash -s' < worker1.sh
-#printf "\n\n"
-#echo "   CONFIGURANDO NODE 2: JOIN"
-#printf "\n\n"
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE2 'bash -s' < worker2.sh
-#printf "\n\n"
-#echo "   CONFIGURANDO NODE 3: JOIN"
-#printf "\n\n"
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE3 'bash -s' < worker3.sh
-
-#printf "\n\n"
-#echo "   VERIFICANDO NODES NO MASTER :"
-#printf "\n\n"
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'docker node ls'
-
-### CONFIGURANDO OS VOLUMES 
-#printf "\n\n"
-#echo "   CONFIGURANDO ETCD no MASTER:"
-#printf "\n\n"
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < config_etcd.sh
-#printf "\n\n"
-#echo "   CONFIGURANDO OS VOLUMES: PORTWORX"
-#printf "\n\n"
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < config_volume_portworx.sh
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE1 'bash -s' < config_volume_portworx.sh
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE2 'bash -s' < config_volume_portworx.sh
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE3 'bash -s' < config_volume_portworx.sh
+### CONCLUINDO
 
 printf "\n\n"
 echo "   CONFIGURAÇÕES REALIZADAS. FIM."
