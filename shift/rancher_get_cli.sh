6a8e30bf31760e42c83970221837e2b644dc6bad
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-07 00:57:19 -0300

    rancher

---
+++ shift/rancher_get_cli.sh
@@ -0,0 +1,7 @@
+# https://rancher.com/docs/rancher/v2.x/en/quick-start-guide/deployment/quickstart-manual-setup/#4-create-the-cluster
+# https://rancher.com/docs/rancher/v2.x/en/cli/
+# https://github.com/rancher/cli/releases
+
+wget https://github.com/rancher/cli/releases/download/v2.4.12/rancher-linux-amd64-v2.4.12.tar.gz
+tar -zxvf rancher-linux-amd64-v2.4.12.tar.gz
+mv rancher-linux-amd64-v2.4.12/rancher /usr/local/bin/
