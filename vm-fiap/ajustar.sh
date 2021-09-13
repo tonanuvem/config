37e6b80ff88a021c62c43fd4103a2f6d6229b9eb
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-13 13:24:00 -0300

    Update ajustar.sh

--- vm-fiap/ajustar.sh
+++ vm-fiap/ajustar.sh
@@ -1,4 +1,9 @@
 terraform output ip_externo > hosts
+
+export ANSIBLE_PYTHON_INTERPRETER=auto_silent
+export ANSIBLE_DEPRECATION_WARNINGS=false
+export ANSIBLE_DISPLAY_SKIPPED_HOSTS=false
+
 ansible-playbook ~/environment/config/ansible/ansible_hostname.yml --extra-vars "nome=vm-fiap" --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
 ansible-playbook ~/environment/config/ansible/ansible_utils.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem &&
 ansible-playbook ~/environment/config/ansible/ansible_docker.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem &&
