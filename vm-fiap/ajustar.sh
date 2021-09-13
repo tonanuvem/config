588ad68af18ba94d6a974101fc2a129036100354
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-13 15:09:37 -0300

    Update ajustar.sh

--- vm-fiap/ajustar.sh
+++ vm-fiap/ajustar.sh
@@ -10,7 +10,7 @@ export ANSIBLE_DEPRECATION_WARNINGS=false
 export ANSIBLE_DISPLAY_SKIPPED_HOSTS=false
 
 ansible-playbook ~/environment/config/ansible/ansible_hostname.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
-ansible-playbook ~/environment/config/ansible/ansible_utils.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem &&
-ansible-playbook ~/environment/config/ansible/ansible_docker.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem &&
-ansible-playbook ~/environment/config/ansible/ansible_k8s.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem &&
+ansible-playbook ~/environment/config/ansible/ansible_utils.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
+ansible-playbook ~/environment/config/ansible/ansible_docker.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
+ansible-playbook ~/environment/config/ansible/ansible_k8s.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
 ansible-playbook ~/environment/config/ansible/ansible_dev_java11.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
