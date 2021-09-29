2bb19fbc7e0bb218be2d0ccf58fae4cabf0a630b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-29 09:16:50 -0300

    Update iniciar.sh

--- ubuntu-vm/iniciar.sh
+++ ubuntu-vm/iniciar.sh
@@ -4,8 +4,9 @@ echo " Iniciando configurações: "
 
 # Aguardando Nodes
 export QTD_NODES=$(terraform output -json ip_externo | jq '.[] | length')
+export WORKER_NODES=$(expr $QTD_NODES - 1)
 
-for N in $(seq 0 $QTD_NODES); do
+for N in $(seq 0 $WORKER_NODES); do
     IP=$(terraform output -json ip_externo | jq .[] | jq .[$N] | sed 's/"//g')
     
     echo "   Aguardando Node $N com $IP: "
