33012264ba50398d8900499128b481c40073bc9a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-29 11:32:41 -0300

    Update conectar.sh

--- ubuntu-vm/conectar.sh
+++ ubuntu-vm/conectar.sh
@@ -1,6 +1,8 @@
 echo "Em qual NODE você deseja conectar? Digitar ex: 0 ou 1 ou 2 (etc)" 
 read NODENUM
-IP=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/ubuntuvm_'$NODENUM'/) ) { print $1} }')
+
+IP=$(terraform output -json ip_externo | jq .[] | jq .[$NODENUM] | sed 's/"//g')
+#IP=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/ubuntuvm_'$NODENUM'/) ) { print $1} }')
 
 echo "Conectando.. IP = $IP"
 ssh ubuntu@${IP} -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem
