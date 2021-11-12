be584578c35a9703cee7617f0ba9f1ad8b4b4813
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-12 08:52:26 -0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 # verificar o tamanho do disco
 
 #sudo python -m pip install ansible
-sudo python3 -m pip install ansible > /dev/null
+sudo python3 -m pip install ansible
 
 printf "\n\tANSIBLE:\n"
 ansible --version
