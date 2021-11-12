5a303c88336d11e42df3cc08fefaf5a5213bc618
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-12 09:32:05 -0300

    Update resize.sh

--- resize.sh
+++ resize.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 # https://docs.aws.amazon.com/cloud9/latest/user-guide/move-environment.html
-# uso: sh resize.sh 20
+# uso: sh resize.sh 100
 
 # Specify the desired volume size in GiB as a command-line argument. If not specified, default to 20 GiB.
-SIZE=${1:-20}
+SIZE=${1:-100}
 
 # Get the ID of the environment host Amazon EC2 instance.
 INSTANCEID=$(curl http://169.254.169.254/latest/meta-data//instance-id)
