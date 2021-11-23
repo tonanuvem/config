0777c6bccdaa335239eb3fed1a35deea7e175ff4
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-23 09:32:33 -0300

    Update k8s_dashboard_token.sh

--- shift/k8s_dashboard_token.sh
+++ shift/k8s_dashboard_token.sh
@@ -4,13 +4,16 @@ export COLOR_LIGHT_GREEN='\e[0;49;32m'
 
 export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/tonanuvem/k8s-exemplos/master/dashboard_permission.yml
 kubectl patch svc kubernetes-dashboard -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
 kubectl get svc kubernetes-dashboard -n kubernetes-dashboard
 export INGRESS_PORT=$(kubectl -n kubernetes-dashboard get service kubernetes-dashboard -o jsonpath='{.spec.ports[?()].nodePort}')
 echo ""
 echo "Acessar K8S Dashboard: https://$INGRESS_HOST:$INGRESS_PORT"
-    
-# Kubernetes dashboard access token.
+
+echo ""
+echo "Kubernetes dashboard access token."
+echo ""
 
 SECRET_RESOURCE=$(kubectl get secrets -n kubernetes-dashboard -o name | grep kubernetes-dashboard-token)
 ENCODED_TOKEN=$(kubectl get $SECRET_RESOURCE -n kubernetes-dashboard -o=jsonpath='{.data.token}')
