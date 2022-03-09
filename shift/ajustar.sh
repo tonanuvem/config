534ccb294cb580208869f8c99eb15766ac73ea5a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-03-09 14:45:56 -0300

    Update ajustar.sh

--- shift/ajustar.sh
+++ shift/ajustar.sh
@@ -47,7 +47,8 @@ done
 
 # aplicar configurações
 #printf  "$MASTER\n$NODE1\n$NODE2\n$NODE3" > hosts
-terraform output -json ip_externo | jq .[0] | jq .[] > hosts
+#terraform output -json ip_externo | jq .[0] | jq .[] > hosts
+
 ansible-playbook ~/environment/config/ansible/ansible_hostname.yml --inventory inv.hosts -u ec2-user --key-file ~/environment/labsuser.pem
 ansible-playbook ~/environment/config/ansible/ansible_hosts.yml --inventory inv.hosts -u ec2-user --key-file ~/environment/labsuser.pem
 ansible-playbook ~/environment/config/ansible/ansible_utils.yml --inventory inv.hosts -u ec2-user --key-file ~/environment/labsuser.pem
