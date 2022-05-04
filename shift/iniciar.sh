07cc4a124b994bd6c217153ecc8aa17ec891e705
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-05-04 17:17:54 -0300

    Update iniciar.sh

--- shift/iniciar.sh
+++ shift/iniciar.sh
@@ -7,18 +7,20 @@ WORKER_NODES=$(expr $QTD_NODES - 1)
 
 # Aguardando Master
 export IP=$MASTER
-echo "   Aguardando Master com $MASTER: "
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { echo .; sleep 1; } done
-echo "   Conectado ao MASTER com IP = $MASTER"
+echo "   Aguardando Master com IP = $MASTER: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao MASTER"
 
 # Aguardando Nodes
 for N in $(seq 1 $WORKER_NODES); do
     NODE=$(terraform output -json ip_externo | jq .[] | jq .[$N] | sed 's/"//g')
-    echo "   Aguardando Node $N: "
-    while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE "echo CONECTADONODE" | grep CONECTADONODE | wc -l) != '1' ]; do { echo .; sleep 1; } done
-    echo "   Conectado ao Node $N com IP = $NODE"
+    echo "   Aguardando Node $N com IP = $NODE: "
+    while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE "echo CONECTADONODE" | grep CONECTADONODE | wc -l) != '1' ]; do { printf .; sleep 1; } done
+    echo "   Conectado ao Node $N"
 done
 
+echo ""
+echo "---"
 echo "   Realizando ajustes (usando o Ansible): "
 sh ajustar.sh
 
