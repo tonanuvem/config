6eec238a0ff912b23d74df463d714c5c604b49e9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-05-04 17:03:13 -0300

    Update iniciar.sh

--- shift/iniciar.sh
+++ shift/iniciar.sh
@@ -12,36 +12,25 @@ echo "NODE2 = $NODE2"
 echo "NODE3 = $NODE3"
 
 # Aguardando Master
-#export IP=$MASTER
-MASTER=$(echo $MASTER)
-NODE1=$(echo $NODE1)
-NODE2=$(echo $NODE2)
-NODE3=$(echo $NODE3)
-
-echo "IPs configurados 2 :"
-echo "MASTER = $MASTER"
-echo "NODE1 = $NODE1"
-echo "NODE2 = $NODE2"
-echo "NODE3 = $NODE3"
-
+export IP=$MASTER
 echo "   Aguardando Node1 com $IP: "
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO0" | grep CONECTADO0 | wc -l) != '1' ]; do { echo .; sleep 1; } done
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { echo .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
 # Aguardando Node1
-#export IP=$NODE1
-echo "   Aguardando Node1 com $NODE1: "
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE1 "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { echo .; sleep 1; } done
-echo "   Conectado ao $NODE1, verificando ajustes: "
+export IP=$NODE1
+echo "   Aguardando Node1 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { echo .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
 # Aguardando Node2
-#export IP=$NODE2
-echo "   Aguardando Node2 com $NODE2: "
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE2 "echo CONECTADO2" | grep CONECTADO2 | wc -l) != '1' ]; do { echo .; sleep 1; } done
-echo "   Conectado ao $NODE2, verificando ajustes: "
+export IP=$NODE2
+echo "   Aguardando Node2 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO2" | grep CONECTADO2 | wc -l) != '1' ]; do { echo .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
 # Aguardando Node3
-#export IP=$NODE3
-echo "   Aguardando Node3 com $NODE3: "
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE3 "echo CONECTADO3" | grep CONECTADO3 | wc -l) != '1' ]; do { echo .; sleep 1; } done
-echo "   Conectado ao $NODE3, verificando ajustes: "
+export IP=$NODE3
+echo "   Aguardando Node3 com $IP: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO3" | grep CONECTADO3 | wc -l) != '1' ]; do { echo .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
 
 sh ajustar.sh
 
