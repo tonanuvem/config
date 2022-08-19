c35a3791f2d0e59ff07bbaea45c159637ef510ee
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-19 08:54:50 -0300

    Update k8s_create_cluster.sh

--- shift/k8s_create_cluster.sh
+++ shift/k8s_create_cluster.sh
@@ -7,35 +7,18 @@ MASTER=$(terraform output -json ip_externo | jq .[] | jq .[0] | sed 's/"//g')
 QTD_NODES=$(terraform output -json ip_externo | jq '.[] | length')
 WORKER_NODES=$(expr $QTD_NODES - 1)
 
-#MASTER=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_0/) ) { print $1} }')
-#NODE1=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_1/) ) { print $1} }')
-#NODE2=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_2/) ) { print $1} }')
-#NODE3=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_3/) ) { print $1} }')
 echo "IPs configurados :"
 echo "MASTER = $MASTER"
-#echo "NODE1 = $NODE1"
-#echo "NODE2 = $NODE2"
-#echo "NODE3 = $NODE3"
-#MASTER=$(~/environment/ip | awk -Fv '/vm_0/{print $1}')
-#NODE1=$(~/environment/ip | awk -Fv '/vm_1/{print $1}')
-#NODE2=$(~/environment/ip | awk -Fv '/vm_2/{print $1}')
-#NODE3=$(~/environment/ip | awk -Fv '/vm_3/{print $1}')
-
-# reset arquivos vazios dos scripts:
 > master.sh
-#> worker1.sh
-#> worker2.sh
-#> worker3.sh
 
 # CONFIGURANDO O MASTER utilizando o KUBEADM INIT:
-#echo "sudo hostnamectl set-hostname master" >> master.sh
+
 echo "Aguardando instalação do KUBEADM."
-#ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER "sudo hostnamectl set-hostname master"
-#ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER "while [ \$(dpkg -l | grep kubeadm | wc -l) != '1' ]; do { printf .; sleep 1; } done"
-#echo "while [ \$(dpkg -l | grep kubeadm | wc -l) != '1' ]; do { printf .; sleep 1; } done" >> master.sh
+
 echo "kubeadm version" >> master.sh
 echo "sudo kubeadm config images pull" >> master.sh
 echo "sudo kubeadm init --control-plane-endpoint \$(curl checkip.amazonaws.com):6443" >> master.sh
+
 ### INICIANDO O MASTER via SSH
 ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < master.sh
 
@@ -43,10 +26,6 @@ ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'b
 echo "mkdir -p $DIRETORIO/.kube" > master.sh
 echo "sudo cp -i /etc/kubernetes/admin.conf $DIRETORIO/.kube/config" >> master.sh
 echo "sudo chown $(id -u):$(id -g) $DIRETORIO/.kube/config" >> master.sh
-#echo "source <(kubectl completion bash)" >> master.sh
-
-# validar
-#cat master.sh
 
 ### CONFIGURANDO O MASTER via SSH
 ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < master.sh
@@ -68,21 +47,8 @@ ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'b
 #    --discovery-token-ca-cert-hash sha256:46abcfc7e371878b78f1071a7e396a3b1f1e851cbec76e65f0030d3f73411fd1
 printf "\n\n"
 echo "CONFIGURANDO OS NODES utilizando o KUBEADM JOIN:"
-#echo "Copie e cole 2 linhas com KUBEADM JOIN exibido acima: (digite ENTER PARA CONCLUIR)"
-#echo "Exemplo das 2 linhas a serem copiadas:"
-#echo " kubeadm join 10.0.1.169:6443 --token fdwf9o.om0jvrom7uv3eeg4 "
-#echo "    --discovery-token-ca-cert-hash sha256:46abcfc7e371878b78f1071a7e396a3b1f1e851cbec76e65f0030d3f73411fd1"
-#read TOKEN
-#read ENTER
-
-#echo "sudo hostnamectl set-hostname worker1" >> worker1.sh
-#echo "sudo hostnamectl set-hostname worker2" >> worker2.sh
-#echo "sudo hostnamectl set-hostname worker3" >> worker3.sh
 
 echo "sudo $TOKEN" > workers.sh
-#echo "sudo $TOKEN" >> worker1.sh
-#echo "sudo $TOKEN" >> worker2.sh
-#echo "sudo $TOKEN" >> worker3.sh
 
 for N in $(seq 1 $WORKER_NODES); do
     printf "\n\n"
@@ -91,21 +57,6 @@ for N in $(seq 1 $WORKER_NODES); do
     ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE 'bash -s' < workers.sh
 done
 
-# validar
-#cat worker1.sh
-#printf "\n\n"
-#echo "   CONFIGURANDO NODE 1: KUBEADM JOIN"
-#printf "\n\n"
-#ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE1 'bash -s' < worker1.sh
-#printf "\n\n"
-#echo "   CONFIGURANDO NODE 2: KUBEADM JOIN"
-#printf "\n\n"
-#ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE2 'bash -s' < worker2.sh
-#printf "\n\n"
-#echo "   CONFIGURANDO NODE 3: KUBEADM JOIN"
-#printf "\n\n"
-#ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE3 'bash -s' < worker3.sh
-#printf "\n\n"
 echo "   VERIFICANDO NODES NO MASTER :"
 printf "\n\n"
 ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'kubectl get nodes'
