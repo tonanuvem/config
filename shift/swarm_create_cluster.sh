b3846003df79f55e96b4ea874cd18581ccb26881
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-19 11:14:15 -0300

    Update swarm_create_cluster.sh

--- shift/swarm_create_cluster.sh
+++ shift/swarm_create_cluster.sh
@@ -14,7 +14,7 @@ printf "\n\n\tMASTER:\n"
 echo ""
 echo "   Aguardando configurações: "
 
-echo "sudo docker swarm init" >> master.sh
+echo "sudo docker swarm init --advertise-addr eth0:2377" >> master.sh
 
 ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < master.sh
 
