5ce6fd4f15262935d122d0bb8b6b7235b7d795cf
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-21 22:46:08 -0300

    Update swarm_create.sh

--- cluster/swarm_create.sh
+++ cluster/swarm_create.sh
@@ -1,24 +1,23 @@
-#NODE1=$(terraform output Node_1_ip_externo)
-MASTER=$(terraform output Node_1_ip_externo)
+NODE1=$(terraform output Node_1_ip_externo)
 NODE2=$(terraform output Node_2_ip_externo)
 NODE3=$(terraform output Node_3_ip_externo)
 
 # Criando arquivos vazios para receber os comandos
-> master.sh
-> workers.sh
+> init.sh
+> token.sh
 
-# CONFIGURANDO O MASTER utilizando o DOCKER SWARM INIT:"
-### CONFIGURANDO O MASTER via SSH
+# CONFIGURANDO O NODE1 utilizando o DOCKER SWARM INIT:"
+### CONFIGURANDO O NODE1 via SSH
 printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
 printf "\n\nNODE 1:\n"
 echo ""
 echo "   Aguardando configurações: "
 
-echo "sudo docker swarm init" >> master.sh
-ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < master.sh
+echo "sudo docker swarm init" >> init.sh
+ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE1 'bash -s' < init.sh
 
 # Get Token
-TOKEN=$(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'docker swarm join-token manager | grep docker')
+TOKEN=$(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE1 'docker swarm join-token manager | grep docker')
 # Remover espacos
 TOKEN=`echo $TOKEN | sed 's/ *$//g'`
 printf "\n\n"
@@ -29,7 +28,7 @@ printf "\n\n"
 
 ### CONFIGURANDO OS NODES utilizando o DOCKER SWARM JOIN:
 
-echo "sudo $TOKEN" >> workers.sh
+echo "sudo $TOKEN" >> token.sh
 
 # Exemplo:
 # docker swarm join --token SWMTKN-1-28amdt0x5r4mbc5092t1w016392emlqv67lyhasph200d6tdhl-41rxupxdsjg9zo00xtdlwon5p 10.1.1.97:2377
@@ -37,18 +36,17 @@ printf "\n\n"
 echo "CONFIGURANDO OS NODES - JOIN:"
 
 echo "   CONFIGURANDO NODE 2 :  JOIN"
-ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE2 'bash -s' < workers.sh
+ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE2 'bash -s' < token.sh
 echo ""
 echo "   CONFIGURANDO NODE 3 :  JOIN"
-ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE3 'bash -s' < workers.sh
+ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE3 'bash -s' < token.sh
 echo ""
 
 printf "\n\n"
 echo "   VERIFICANDO NODES :"
 printf "\n\n"
-ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'docker node ls'
+ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE1 'docker node ls'
 
 printf "\n\n"
 echo "   CONFIGURAÇÕES REALIZADAS. FIM."
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'docker node ls'
 printf "\n\n"
