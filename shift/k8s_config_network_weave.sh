0485ea92b6123552c891f6122d905f9f2414f94e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-10-26 18:20:46 -0300

    Update k8s_config_network_weave.sh

--- shift/k8s_config_network_weave.sh
+++ shift/k8s_config_network_weave.sh
@@ -1,10 +1,12 @@
 # Configurar a rede (pod network) no Master:
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 # Executar Ferramenta Gráfica de administração da Weave Works:
 
-kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-service-type=NodePort&k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://github.com/weaveworks/scope/releases/download/v1.13.2/k8s-scope.yaml
+#kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-service-type=NodePort&k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 kubectl patch svc weave-scope-app -n weave -p \
   '{"spec": { "type": "NodePort", "ports": [ { "nodePort": 32040, "port": 80, "protocol": "TCP", "targetPort": 4040 } ] } }'
