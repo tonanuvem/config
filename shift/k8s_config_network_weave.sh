7015eea88997dff5b50925b21e21ffa54491b706
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-10-26 18:25:41 -0300

    Update k8s_config_network_weave.sh

--- shift/k8s_config_network_weave.sh
+++ shift/k8s_config_network_weave.sh
@@ -5,7 +5,7 @@ kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/we
 
 # Executar Ferramenta Gráfica de administração da Weave Works:
 
-kubectl apply -f https://github.com/weaveworks/scope/releases/download/v1.13.2/k8s-scope.yaml
+kubectl apply -f https://github.com/weaveworks/scope/releases/download/v1.13.2/k8s-scope.yaml?k8s-service-type=NodePort
 #kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-service-type=NodePort&k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 kubectl patch svc weave-scope-app -n weave -p \
