32e34dacec1a128ae4a528882b5f240e6721a317
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-10-31 11:05:19 -0300

    Update k8s_dashboard_token.sh

--- shift/k8s_dashboard_token.sh
+++ shift/k8s_dashboard_token.sh
@@ -23,28 +23,10 @@ echo ""
 echo ""
 
 ################ Comandos antigos, pois desabilitei o Token
+#kubectl apply -f https://raw.githubusercontent.com/tonanuvem/k8s-exemplos/master/dashboard_adminuser.yml
+#export TOKEN=$(kubectl -n kubernetes-dashboard create token admin-user)
 #echo ""
-#echo "Kubernetes dashboard access token."
-#echo ""
-
-#cat <<EOF | kubectl apply -f -
-#apiVersion: v1
-#kind: Secret
-#type: kubernetes.io/service-account-token
-#metadata:
-#  name: kubernetes-dashboard-token
-#  namespace: kubernetes-dashboard
-#  annotations:
-#    kubernetes.io/service-account.name: "kubernetes-dashboard"
-#  labels:
-#    k8s-app: kubernetes-dashboard
-#EOF
-
-# kubectl describe -n kubernetes-dashboard secret kubernetes-dashboard-token
-#ENCODED_TOKEN=$(kubectl get secret kubernetes-dashboard-token -n kubernetes-dashboard -o=jsonpath='{.data.token}')
-#export TOKEN=$(echo $ENCODED_TOKEN | base64 --decode)
-#echo ""
-#echo "--- Copy and paste this token for dashboard access ---"
+#echo "--- Copy and paste : token for dashboard access ---"
 #echo -e $COLOR_LIGHT_GREEN
 #echo -e $TOKEN
 #echo -e $COLOR_RESET
