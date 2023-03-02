df03c68e9b1f47d9a3d8e056d5e270014622aded
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-03-02 11:57:33 -0300

    Update aws_cli_eks_create.sh

--- eks/aws_cli_eks_create.sh
+++ eks/aws_cli_eks_create.sh
@@ -1,25 +1,29 @@
 # https://blog.devgenius.io/how-to-create-eks-cluster-using-aws-cli-60e41111940b
 
+mkdir ./awscli
+
 # Criar o VPC
 
 VPC_ID=$(aws ec2 create-vpc --cidr-block 10.6.0.0/16 --query Vpc.VpcId --output text)
 
 echo ""
 echo "Criado o VPC : $VPC_ID"
-echo $VPC_ID >> VPC_ID_EKS.txt
+echo $VPC_ID >> ./awscli/VPC_ID.txt
 echo ""
 
 # Criar as subredes
 
 SUBNET_1_ID=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block 10.6.1.0/24 --availability-zone us-east-1a --query Subnet.SubnetId --output text)
 echo "Criado o Subnet 1 : $SUBNET_1_ID"
+echo $SUBNET_1_ID >> ./awscli/SUBNET_1_ID.txt
 SUBNET_2_ID=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block 10.6.2.0/24 --availability-zone us-east-1b --query Subnet.SubnetId --output text)
 echo "Criado o Subnet 2 : $SUBNET_2_ID"
+echo $SUBNET_2_ID >> ./awscli/SUBNET_2_ID.txt
 
 
 # Criar as regras de firewall
 
-SECURITYGROUP_ID=$(aws ec2 create-security-group --group-name eks-node-group --description "EKS Node Group" --vpc-id $VPC_ID --query GroupId --output text)
+SECURITYGROUP_ID=$(aws ec2 create-security-group --group-name eks-sg-node-group --description "EKS Node Group" --vpc-id $VPC_ID --query GroupId --output text)
 
 aws ec2 authorize-security-group-ingress --group-id $SECURITYGROUP_ID --protocol "all" --cidr "0.0.0.0/0"
 #aws ec2 authorize-security-group-ingress --group-id $SECURITYGROUP_ID --protocol tcp --port 22 --cidr 0.0.0.0/0 
@@ -28,6 +32,7 @@ aws ec2 authorize-security-group-ingress --group-id $SECURITYGROUP_ID --protocol
 
 echo ""
 echo "Criadas as regras de firewall : $SECURITYGROUP_ID"
+echo $SECURITYGROUP_ID >> ./awscli/SECURITYGROUP_ID.txt
 echo ""
 
 # Criar o Cluster EKS
@@ -48,7 +53,7 @@ echo "   Cluster criado! "
 
 # Criar os Nodes
 
-aws eks create-nodegroup --cluster-name eksfiap --nodegroup-name workers_eksfiap --scaling-config minSize=1,maxSize=4,desiredSize=2 --subnets $SUBNET_1_ID,$SUBNET_2_ID --remote-access ec2SshKey=vockey,sourceSecurityGroups=$SECURITYGROUP_ID --instance-types t3.medium --node-role LabRole
+aws eks create-nodegroup --cluster-name eksfiap --nodegroup-name workers_eksfiap --scaling-config minSize=1,maxSize=4,desiredSize=2 --subnets $SUBNET_1_ID,$SUBNET_2_ID --remote-access ec2SshKey=vockey,sourceSecurityGroups=$SECURITYGROUP_ID --instance-types t3.medium --node-role $ROLE_ARN
 # --ami-type AL2_x86_64
 aws eks describe-nodegroup --cluster-name eksfiap --nodegroup-name workers_eksfiap
 
