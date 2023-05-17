2aacf6fa5dcfc06bce5532d5b99cfb4b4b3f9da9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-05-17 09:06:09 -0300

    Update rancher_create_cluster.sh

--- shift/rancher_create_cluster.sh
+++ shift/rancher_create_cluster.sh
@@ -1,30 +1,24 @@
 # conectar no master e configurar
 # https://rancher.com/adding-custom-nodes-kubernetes-cluster-rancher-2-0-tech-preview-2
 
-MASTER=$(terraform output -json ip_externo | jq .[] | jq .[0] | sed 's/"//g')
+RANCHER_SERVER=$(terraform output -json ip_externo | jq .[] | jq .[0] | sed 's/"//g')
 QTD_NODES=$(terraform output -json ip_externo | jq '.[] | length')
 WORKER_NODES=$(expr $QTD_NODES - 1)
 
-> master.sh
-
-# CONFIGURANDO O MASTER utilizando o DOCKER SWARM INIT:"
-### CONFIGURANDO O MASTER via SSH
+### CONFIGURANDO via SSH
 printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
-printf "\n\n\tMASTER:\n"
+printf "\n\n\tRANCHER_SERVER:\n"
 echo ""
 echo "   Aguardando configurações: "
 
-#echo "sudo docker run --privileged -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher" >> master.sh
-
-ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < rancher_server.sh
+ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$RANCHER_SERVER 'bash -s' < rancher_server.sh
 
 # Get Token
-DOCKERRUNCMD=$(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'cat DOCKERRUNCMD')
+DOCKERRUNCMD=$(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$RANCHER_SERVER 'cat DOCKERRUNCMD')
 # Echo command
 echo $DOCKERRUNCMD
 TOKEN=$DOCKERRUNCMD
 
-#TOKEN=$(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'docker swarm join-token manager | grep docker')
 # Remover espacos
 TOKEN=`echo $TOKEN | sed 's/ *$//g'`
 printf "\n\n"
@@ -33,7 +27,7 @@ printf "\n\n"
 echo "   TOKEN ACIMA : CLUSTER JOIN"
 printf "\n\n"
 
-### CONFIGURANDO OS NODES utilizando o DOCKER SWARM JOIN:
+### CONFIGURANDO OS NODES :
 
 echo $TOKEN > workers.sh
 
@@ -51,6 +45,10 @@ done
 
 printf "\n\n"
 echo "   CONFIGURAÇÕES REALIZADAS. FIM."
-echo "   Acessar Rancher URL = http://fiap.${MASTER}.nip.io"
-#ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'docker node ls'
+echo "   Acessar Rancher URL = http://fiap.${RANCHER_SERVER}.nip.io"
+echo ""
+echo ""
+echo "   Senha de Bootstrap:"
+echo ""
+ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$RANCHER_SERVER 'docker logs rancherserver 2>&1 | grep "Bootstrap Password:"'
 printf "\n\n"
