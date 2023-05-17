2a6aa9240a328d30e7f87205bb470f68893d0eb5
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-05-17 14:22:50 -0300

    Update rancher_join_token.sh

--- shift/rancher_join_token.sh
+++ shift/rancher_join_token.sh
@@ -2,12 +2,12 @@
 CLUSTERID=$(cat CLUSTERID)
 APITOKEN=$(cat APITOKEN)
 echo "  Aguardando Token para o Cluster "
-CMD=$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken?id="'$CLUSTERID'"' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.data[].nodeCommand' | head -1)
-while [ $CMD != "1" ]; do
-  printf "."
-  CMD=$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken?id="'$CLUSTERID'"' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.data[].nodeCommand' | head -1)
-  sleep 1
-done
+#CMD=$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken?id="'$CLUSTERID'"' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.data[].nodeCommand' | head -1)
+#while [ ($CMD|wc -l) != "1" ]; do
+#  printf "."
+#  CMD=$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken?id="'$CLUSTERID'"' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.data[].nodeCommand' | head -1)
+#  sleep 1
+#done
 sleep 1
 AGENTCMD=$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken?id="'$CLUSTERID'"' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.data[].nodeCommand' | head -1)
 #printf "\n\n\tAGENTCMD = $AGENTCMD"
