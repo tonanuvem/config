e9195f72d5ec982bc3792df6ee55f410bdc560a2
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-05-17 14:34:35 -0300

    Update rancher_get_cli.sh

--- shift/rancher_get_cli.sh
+++ shift/rancher_get_cli.sh
@@ -5,3 +5,11 @@
 wget https://github.com/rancher/cli/releases/download/v2.4.12/rancher-linux-amd64-v2.4.12.tar.gz
 tar -zxvf rancher-linux-amd64-v2.4.12.tar.gz
 sudo mv rancher-v2.4.12/rancher /usr/local/bin/
+
+APITOKEN=$(cat APITOKEN)
+CLUSTERID=$(cat APITOKEN)
+
+rancher login https://localhost -t $APITOKEN --skip-verify
+rancher clusters create exemplo
+rancher clusters add-node --etcd --controlplane --worker exemplo > JOINTOKEN
+#rancher clusters add-node --etcd --controlplane --worker $$APITOKEN fiap
