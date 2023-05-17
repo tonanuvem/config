123388dc0076bbe19d0139a5be61bd8556d499ce
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-05-17 13:56:05 -0300

    Update rancher_config_server.sh

--- shift/rancher_config_server.sh
+++ shift/rancher_config_server.sh
@@ -44,25 +44,26 @@ echo "CLUSTERID = $CLUSTERID"
 # echo "Criando o token"
 # curl -s 'https://127.0.0.1/v3/clusterregistrationtoken' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"clusterRegistrationToken","clusterId":"'$CLUSTERID'"}' --insecure
 
+# techo abaixo
+# inserido no arquivo rancher_join_token.sh:
+
 # Get nodecommand
-CLUSTERID=$(cat CLUSTERID)
-APITOKEN=$(cat APITOKEN)
-echo "  Aguardando Token para o Cluster "
-while [ "$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken/' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | grep nodeCommand | wc -l)" != "1" ]; do
-  printf "."
-  sleep 1
-done
-sleep 5
-AGENTCMD=$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken?id="'$CLUSTERID'"' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.data[].nodeCommand' | head -1)
+#CLUSTERID=$(cat CLUSTERID)
+#APITOKEN=$(cat APITOKEN)
+#echo "  Aguardando Token para o Cluster "
+#while [ "$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken/' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | grep nodeCommand | wc -l)" != "1" ]; do
+#  printf "."
+#  sleep 1
+#done
+#sleep 5
+#AGENTCMD=$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken?id="'$CLUSTERID'"' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.data[].nodeCommand' | head -1)
 #printf "\n\n\tAGENTCMD = $AGENTCMD"
-echo $AGENTCMD > AGENTCMD
-
-printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
-
+#echo $AGENTCMD > AGENTCMD
+#printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
 # Concat commands
 # Set role flags
-AGENTCMD=$(cat AGENTCMD)
-ROLEFLAGS="--etcd --controlplane --worker"
-DOCKERRUNCMD="$AGENTCMD $ROLEFLAGS"
+#AGENTCMD=$(cat AGENTCMD)
+#ROLEFLAGS="--etcd --controlplane --worker"
+#DOCKERRUNCMD="$AGENTCMD $ROLEFLAGS"
 echo "$DOCKERRUNCMD" > DOCKERRUNCMD
 
