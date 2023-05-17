81e3032e86fa3e799e1a6e66ad58ab92fa70ded7
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-05-17 12:14:45 -0300

    Update rancher_config_server.sh

--- shift/rancher_config_server.sh
+++ shift/rancher_config_server.sh
@@ -44,11 +44,11 @@ echo "CLUSTERID = $CLUSTERID"
 # echo "Criando o token"
 # curl -s 'https://127.0.0.1/v3/clusterregistrationtoken' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"clusterRegistrationToken","clusterId":"'$CLUSTERID'"}' --insecure
 
-# Generate nodecommand
+# Get nodecommand
 CLUSTERID=$(cat CLUSTERID)
 APITOKEN=$(cat APITOKEN)
 echo "  Aguardando Token para o Cluster "
-while [ "$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken?id="'$CLUSTERID'"' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.data[].nodeCommand' | head -1 | wc -l))" != "1" ]; do
+while [ "$(curl -s 'https://127.0.0.1/v3/clusterregistrationtoken/'$CLUSTERID':default-token' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --insecure | jq -r '.nodeCommand' | wc -l))" != "1" ]; do
   printf "."
   sleep 1
 done
