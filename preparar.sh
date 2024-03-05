864e1127eccd7dc49991aaa9dea8d03d33a3bc90
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-03-05 18:39:36 -0300

    Update preparar.sh

--- preparar.sh
+++ preparar.sh
@@ -4,18 +4,6 @@ echo " Aumentando o tamanho do disco para 100G, podem aparecer Warnings"
 # aumentando o disco para 100G e 
 sh ~/environment/config/resize.sh 100 > /dev/null
 
-echo " Liberando firewall para o ambiente do Cloud9"
-if [ $(aws ec2 describe-security-groups | jq '.SecurityGroups[] | select(.GroupName | contains("cloud9")) | .GroupName' | wc -l) = "1" ]
-then
-  # liberar firewall automaticamente se só existe 1 security group
-  NOME_GRUPO_SEGURANCA=$(aws ec2 describe-security-groups | jq '.SecurityGroups[] | select(.GroupName | contains("cloud9")) | .GroupName' | tr -d \")
-  aws ec2 authorize-security-group-ingress --group-name $NOME_GRUPO_SEGURANCA --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
-else
-  # escolher manualmente dentre os securty groups existentes
-  bash ~/environment/config/firewall_allow.sh
-fi
-#sh ~/environment/config/resize.sh 100 > /dev/null
-
 echo "\n\n Configurar pre-req para instalação do Ansible"
 # configurar pre-req (inventario) ansible
 export VM=$(curl -s checkip.amazonaws.com)
@@ -42,7 +30,17 @@ echo "\n\n Configurar Cloud9 com Ansible"
 ansible-playbook ~/environment/config/ansible/cloud9.yml --inventory hosts 
   #-u ubuntu --key-file ~/environment/labsuser.pem
 
-ansible-playbook ~/environment/config/ansible/ansible_verificar.yml --inventory hosts
+echo " Liberando firewall para o ambiente do Cloud9"
+if [ $(aws ec2 describe-security-groups | jq '.SecurityGroups[] | select(.GroupName | contains("cloud9")) | .GroupName' | wc -l) = "1" ]
+then
+  # liberar firewall automaticamente se só existe 1 security group
+  NOME_GRUPO_SEGURANCA=$(aws ec2 describe-security-groups | jq '.SecurityGroups[] | select(.GroupName | contains("cloud9")) | .GroupName' | tr -d \")
+  aws ec2 authorize-security-group-ingress --group-name $NOME_GRUPO_SEGURANCA --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
+else
+  # escolher manualmente dentre os securty groups existentes
+  bash ~/environment/config/firewall_allow.sh
+fi
+
 
 ## FIM
 source ~/.bash_profile
