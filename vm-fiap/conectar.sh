2b6b275f7f805fe6af1e3d2561daee9527df546b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-05-06 12:43:30 -0300

    Update conectar.sh

--- vm-fiap/conectar.sh
+++ vm-fiap/conectar.sh
@@ -1,6 +1,6 @@
 # conectar
 
-IP=$(terraform output ip_externo)
+IP=$(terraform output -raw ip_externo)
 
 echo "Conectando.. IP = $IP.."
 ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP
