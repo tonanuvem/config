c88b461c4a0427eab6afadd7a62c7e8e4a7687ef
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-05-06 12:59:59 -0300

    Update config-node2.sh

--- cluster/config-node2.sh
+++ cluster/config-node2.sh
@@ -6,7 +6,7 @@ printf "\n\n\tNODE 2: configurando hostname\n\n"
 echo ""
 echo "   Aguardando configurações: "
 sleep 1
-export IP=$(terraform output Node_2_ip_externo)
+export IP=$(terraform output -raw Node_2_ip_externo)
 while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
 ssh -o LogLevel=error -i ~/environment/labsuser.pem ec2-user@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
