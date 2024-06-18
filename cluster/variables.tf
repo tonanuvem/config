a515225191ff7df2d447367c1dbd96e06cfc8b49
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-06-18 10:46:00 -0300

    Update variables.tf

--- cluster/variables.tf
+++ cluster/variables.tf
@@ -36,6 +36,7 @@ variable "aws_region" {
 variable "aws_amis" {
   default = {
     # us-east-1 = "ami-0c2b8ca1dad447f8a" # amazon linux 1
-    us-east-1 = "ami-0e8a34246278c21e4" # amazon linux 2 (v. 202311)
+    # us-east-1 = "ami-0e8a34246278c21e4" # amazon linux 2 (v. 202311 - end of support)
+    us-east-1 = "ami-08a0d1e16fc3f61ea"   # amazon linux 2023 (v. 2003.4 20240611)
   }
 }
