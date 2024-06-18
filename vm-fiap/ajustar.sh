17f94c3c5591ab50f1bab263619ef036309a14d2
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-06-18 12:09:07 -0300

    Update ajustar.sh

--- vm-fiap/ajustar.sh
+++ vm-fiap/ajustar.sh
@@ -13,4 +13,4 @@ ansible-playbook ~/environment/config/ansible/ansible_hostname.yml --inventory h
 ansible-playbook ~/environment/config/ansible/ansible_utils.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
 ansible-playbook ~/environment/config/ansible/ansible_docker.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
 ansible-playbook ~/environment/config/ansible/ansible_k8s.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
-ansible-playbook ~/environment/config/ansible/ansible_dev_java11.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
+ansible-playbook ~/environment/config/ansible/ansible_dev_java.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
