46f9540b21f0f0adfb20e9f06abc9a22b8991e96
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-08-02 01:55:47 -0300

    Update and rename kasm.sh to desktop_kams.sh

--- desktop_kams.sh
+++ desktop_kams.sh
@@ -2,6 +2,8 @@
 # https://kasmweb.com/docs/latest/install/system_requirements.html#operating-system
 # https://kasmweb.com/docs/latest/install/single_server_install.html
 
+# https://www.youtube.com/watch?v=JkYF20Kq4G8
+
 cd /tmp
 curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.15.0.06fdc8.tar.gz
 tar -xf kasm_release_1.15.0.06fdc8.tar.gz
