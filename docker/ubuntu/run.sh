efb20646e09110ffd4f1963e140cd22406244991
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-08-02 01:26:31 -0300

    Update run.sh

--- docker/ubuntu/run.sh
+++ docker/ubuntu/run.sh
@@ -1,4 +1,4 @@
 #docker run --hostname fiap --name webconfig --rm -p 4200:4200 -d config /bin/bash
-docker stop webconfig
+docker stop webconfig && git pull
 docker run --rm -ti --name webconfig -d config /bin/bash 
 docker exec -ti webconfig /bin/bash
