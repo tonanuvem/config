3e36a6cc9492a507f14c9fcdcd4b675bb8f9cd8b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-08-02 02:54:03 -0300

    Update cloudshell.sh

--- cloudshell.sh
+++ cloudshell.sh
@@ -10,10 +10,10 @@ HOST=$(echo "$AWS_CONTAINER_CREDENTIALS_FULL_URI" | sed 's|/latest.*||')
 TOKEN=$(curl -s -X PUT "$HOST"/latest/api/token -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
 OUTPUT=$(curl -s "$HOST/latest/meta-data/container/security-credentials" -H "X-aws-ec2-metadata-token: $TOKEN")
 echo "[default]" > ~/credentials
-echo "AWS_ACCESS_KEY_ID=$(echo "$OUTPUT" | jq -r '.AccessKeyId')" > ~/credentials
-echo "AWS_SECRET_ACCESS_KEY=$(echo "$OUTPUT" | jq -r '.SecretAccessKey')" > ~/credentials
-echo "AWS_SESSION_TOKEN=$(echo "$OUTPUT" | jq -r '.Token')" > ~/credentials
-echo "region=us-east-1" > ~/credentials
+echo "AWS_ACCESS_KEY_ID=$(echo "$OUTPUT" | jq -r '.AccessKeyId')" >> ~/credentials
+echo "AWS_SECRET_ACCESS_KEY=$(echo "$OUTPUT" | jq -r '.SecretAccessKey')" >> ~/credentials
+echo "AWS_SESSION_TOKEN=$(echo "$OUTPUT" | jq -r '.Token')" >> ~/credentials
+echo "region=us-east-1" >> ~/credentials
 
 # INICIANDO COMENTARIO
 : <<'END'
