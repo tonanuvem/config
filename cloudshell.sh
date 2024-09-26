ccffa4526d7b7946074632bdf16fa3a4cbbde490
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-09-26 09:50:42 -0300

    Update cloudshell.sh

--- cloudshell.sh
+++ cloudshell.sh
@@ -15,6 +15,8 @@ echo "AWS_SECRET_ACCESS_KEY=$(echo "$OUTPUT" | jq -r '.SecretAccessKey')" >> ~/c
 echo "AWS_SESSION_TOKEN=$(echo "$OUTPUT" | jq -r '.Token')" >> ~/credentials
 echo "region=us-east-1" >> ~/credentials
 
+# sh terraform.sh
+
 # CRIAR UBUNTU VM DO CODESERVER
 # https://docs.aws.amazon.com/cli/v1/userguide/cli-services-ec2-instances.html
 # aws ec2 run-instances --image-id ami-0bf6b162dbe07782b --key-name vockey --instance-type m5.large
