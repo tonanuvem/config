ed70a286dd13d91c049c6b3b17e8a3e4d7763b7d
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-09-26 09:49:27 -0300

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -3,7 +3,7 @@
 # Atualizar versao do Terraform: 
 printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
 printf "\n\n\tTerraform:\n\n"
-curl -s "https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip" -o "terraform_linux_amd64.zip"
+curl -s "https://releases.hashicorp.com/terraform/1.9.5/terraform_1.9.5_linux_amd64.zip" -o "terraform_linux_amd64.zip"
 unzip terraform_linux_amd64.zip
 sudo mv terraform /usr/bin/
 
