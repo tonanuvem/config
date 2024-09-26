fd684bcf7f2085fb5da5c2809981cad86c5ada91
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-09-26 14:02:23 -0300

    Update conectar.sh

--- ubuntu-vm/conectar.sh
+++ ubuntu-vm/conectar.sh
@@ -1,5 +1,9 @@
 #echo "Em qual NODE você deseja conectar? Digitar ex: 0 ou 1 ou 2 (etc)" 
 #read NODENUM
+
+echo "Conectando ..." 
+terraform apply --auto-approve > /dev/null
+
 NODENUM=0
 
 IP=$(terraform output -json ip_externo | jq .[] | jq .[$NODENUM] | sed 's/"//g')
