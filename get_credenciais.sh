b15d9a4c6174f96560546cd4f9cc39a3026361f8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-09-26 22:47:31 -0300

    Update and rename credenciais.sh to get_credenciais.sh

--- get_credenciais.sh
+++ get_credenciais.sh
@@ -1,4 +1,6 @@
 cd ~
+
+# Credenciais
 HOST=$(echo "$AWS_CONTAINER_CREDENTIALS_FULL_URI" | sed 's|/latest.*||')
 TOKEN=$(curl -s -X PUT "$HOST"/latest/api/token -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
 OUTPUT=$(curl -s "$HOST/latest/meta-data/container/security-credentials" -H "X-aws-ec2-metadata-token: $TOKEN")
@@ -7,3 +9,6 @@ echo "AWS_ACCESS_KEY_ID=$(echo "$OUTPUT" | jq -r '.AccessKeyId')" >> ~/credentia
 echo "AWS_SECRET_ACCESS_KEY=$(echo "$OUTPUT" | jq -r '.SecretAccessKey')" >> ~/credentials
 echo "AWS_SESSION_TOKEN=$(echo "$OUTPUT" | jq -r '.Token')" >> ~/credentials
 echo "region=us-east-1" >> ~/credentials
+
+# Terraform
+sudo cp ~/terraform /usr/bin/
