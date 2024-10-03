71708a57f632fb032c0a97cd67288fe294353b1e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-10-02 22:58:51 -0300

    Update destruir.sh

--- eks/destruir.sh
+++ eks/destruir.sh
@@ -1,1 +1,13 @@
+EKS_VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[].VpcId" --output text --filters 'Name=tag:Name,Values=*eksfiap')
+echo "EKS_VPC_ID = $EKS_VPC_ID. Finalizando os recursos..."
+echo ""
+chmod +x delete_vpc.sh
+./delete_vpc.sh --region us-east-1 --vpc-id $EKS_VPC_ID --non-interactive 
+# https://github.com/lianghong/delete_vpc/blob/master/delete_vpc.sh
+
+echo ""
+echo ""
+echo "Finalizando o restante do projeto"
+echo ""
+
 terraform destroy -auto-approve
