450df3dc5f14a32cd22fb6ad805212225ec5a056
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-03-12 10:46:01 -0300

    Update chrome_docker.sh

--- chrome_docker.sh
+++ chrome_docker.sh
@@ -8,7 +8,7 @@ docker run --rm --name chrome -p 5900:5900 --net netchrome -e VNC_SCREEN_SIZE=10
 docker run --rm --name novnc -p 6080:6080 --net netchrome -e AUTOCONNECT=true -e VNC_SERVER=chrome:5900 -d bonigarcia/novnc:1.2.0
 
 echo ""
-echo "Chrome executado. Para colar no Navegador, usar o botão da Área de Transferência do menu esquerdo"
+echo "Chrome executado. Para colar no Navegador, usar o botão da Área de Transferência do menu esquerdo do noVNC"
 echo ""
 ip=$(curl -s checkip.amazonaws.com)
 echo ""
