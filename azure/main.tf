6da741188e389cfb3d8633155e1703610c553c4e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-08 00:37:51 -0300

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -119,7 +119,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_DS1_v2"
+  size                  = var.vm_size
 
   os_disk {
     name                 = "myOsDisk"
