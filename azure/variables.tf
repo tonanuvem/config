d61099dc8d6f248599b0de19a23ccb43eba34a7d
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-07 21:27:20 -0300

    Update variables.tf

--- azure/variables.tf
+++ azure/variables.tf
@@ -1,4 +1,11 @@
 variable "location" {
-  type    = string
-  default = "East US"
+  type        = string
+  default     = "East US"
+  description = "Localização do grupo de recursos."
+}
+
+variable "username" {
+  type        = string
+  description = "Nome de usuário para a conta local na nova VM."
+  default     = "azureuser"
 }
