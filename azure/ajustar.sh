f5395fb746938d404ee80bd09175c9e350a04975
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-07 23:50:20 -0300

    Create ajustar.sh

---
+++ azure/ajustar.sh
@@ -0,0 +1,16 @@
+export VM=$(terraform output -raw ip_externo)
+
+# configurar inventario ansible
+echo '[nodes]' > hosts
+echo "vm-fiap ansible_ssh_host=$VM" >> hosts
+echo '' >> hosts
+
+export ANSIBLE_PYTHON_INTERPRETER=auto_silent
+export ANSIBLE_DEPRECATION_WARNINGS=false
+export ANSIBLE_DISPLAY_SKIPPED_HOSTS=false
+
+ansible-playbook ~/environment/config/ansible/ansible_hostname.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem # --extra-vars "checar_Ambiente=sim"
+ansible-playbook ~/environment/config/ansible/ansible_utils.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
+ansible-playbook ~/environment/config/ansible/ansible_docker.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
+ansible-playbook ~/environment/config/ansible/ansible_k8s.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
+ansible-playbook ~/environment/config/ansible/ansible_dev_java.yml --inventory hosts -u ec2-user --key-file ~/environment/labsuser.pem
