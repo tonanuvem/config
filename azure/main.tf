bdbc48d34266dfce69a050d7ace26e90c3d660ed
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-07 23:24:29 -0300

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -21,13 +21,14 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
+#resource "random_pet" "rg_name" {
+#  prefix = var.resource_group_name_prefix
+#}
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  name     = "fiap"
+  #name     = random_pet.rg_name.id
 }
 
 # Create virtual network
