89d3695416722b6523c0320d8c4226a23ba17550
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-07 22:40:59 -0300

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.27.0"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
 provider "azurerm" {
   features {}
 }
