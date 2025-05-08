cfad01d8665dadf2ae1f3dc4094a2f608e4da98c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-08 00:51:16 -0300

    Update output.tf

--- azure/output.tf
+++ azure/output.tf
@@ -1,7 +1,7 @@
 output "dns_externo" {
-  value = azurerm_public_ip.my_terraform_public_ip.fqdn
+  value = azurerm_public_ip.fiaplab_public_ip.fqdn
 }
 
 output "ip_externo" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+  value = azurerm_linux_virtual_machine.fiaplab_vm.public_ip_address
 }
