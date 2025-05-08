d05fa8f45b3b4e9ae9158599c41221250eed92ea
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-08 00:49:32 -0300

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -21,18 +21,13 @@ provider "azurerm" {
   features {}
 }
 
-#resource "random_pet" "rg_name" {
-#  prefix = var.resource_group_name_prefix
-#}
-
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = "fiap"
-  #name     = random_pet.rg_name.id
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "my_terraform_network" {
+resource "azurerm_virtual_network" "fiaplab_network" {
   name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
@@ -40,15 +35,15 @@ resource "azurerm_virtual_network" "my_terraform_network" {
 }
 
 # Create subnet
-resource "azurerm_subnet" "my_terraform_subnet" {
+resource "azurerm_subnet" "fiaplab_subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  virtual_network_name = azurerm_virtual_network.fiaplab_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Create public IPs
-resource "azurerm_public_ip" "my_terraform_public_ip" {
+resource "azurerm_public_ip" "fiaplab_public_ip" {
   name                = var.public_ip_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -56,7 +51,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
 }
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "my_terraform_nsg" {
+resource "azurerm_network_security_group" "fiaplab_nsg" {
   name                = var.nsg_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -75,29 +70,28 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
 }
 
 # Create network interface
-resource "azurerm_network_interface" "my_terraform_nic" {
+resource "azurerm_network_interface" "fiaplab_nic" {
   name                = var.nic_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "my_nic_configuration"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    subnet_id                     = azurerm_subnet.fiaplab_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.fiaplab_public_ip.id
   }
 }
 
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
-  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+  network_interface_id      = azurerm_network_interface.fiaplab_nic.id
+  network_security_group_id = azurerm_network_security_group.fiaplab_nsg.id
 }
 
 # Generate random text for a unique storage account name
 resource "random_id" "random_id" {
   keepers = {
-    # Generate a new ID only when a new resource group is defined
     resource_group = azurerm_resource_group.rg.name
   }
 
@@ -105,7 +99,7 @@ resource "random_id" "random_id" {
 }
 
 # Create storage account for boot diagnostics
-resource "azurerm_storage_account" "my_storage_account" {
+resource "azurerm_storage_account" "fiaplab_storage_account" {
   name                     = "${var.storage_account_prefix}"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
@@ -114,11 +108,11 @@ resource "azurerm_storage_account" "my_storage_account" {
 }
 
 # Create virtual machine
-resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+resource "azurerm_linux_virtual_machine" "fiaplab_vm" {
   name                  = var.vm_name
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  network_interface_ids = [azurerm_network_interface.fiaplab_nic.id]
   size                  = var.vm_size
 
   os_disk {
@@ -134,20 +128,12 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
     version   = "latest"
   }
 
-  computer_name                  = var.vm_name
-  admin_username                 = var.username
+  computer_name                   = var.vm_name
+  admin_username                  = var.username
   disable_password_authentication = false
-  admin_password                 = var.senha
-
-  #admin_ssh_key {
-  #  username   = var.username
-  #  public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
-  #}
-  
+  admin_password                  = var.senha
 
   boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+    storage_account_uri = azurerm_storage_account.fiaplab_storage_account.primary_blob_endpoint
   }
 }
-
-
