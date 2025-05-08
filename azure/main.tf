1b6a14090c2bac054c4f59c28b3974b725d6e906
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-07 23:13:03 -0300

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 provider "azurerm" {
   features {}
 }
@@ -27,25 +50,13 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "AllowAllInbound"
-    priority                   = 100
+    name                       = "AllowSSH"
+    priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "*"
+    protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "AllowAllOutbound"
-    priority                   = 101
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
@@ -61,10 +72,29 @@ resource "azurerm_network_interface" "nic" {
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
+
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
+resource "azurerm_public_ip" "public_ip" {
+  name                = "devlabs-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "devlabs-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
@@ -73,15 +103,19 @@ resource "azurerm_linux_virtual_machine" "vm" {
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
   size                  = "Standard_B2as_v2"
-  admin_username        = "ubuntu"
+  admin_username        = "azureuser"
   network_interface_ids = [azurerm_network_interface.nic.id]
-  disable_password_authentication = false
-  admin_password        = "P@ssword1234!"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
 
   os_disk {
+    name                 = "devlabs-osdisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    name                 = "devlabs-osdisk"
   }
 
   source_image_reference {
@@ -92,7 +126,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.diag.primary_blob_endpoint
+    enabled = true
+    storage_uri = azurerm_storage_account.diag.primary_blob_endpoint
   }
 }
 
@@ -103,3 +138,4 @@ resource "azurerm_storage_account" "diag" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
