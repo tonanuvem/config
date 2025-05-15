6e8ab8cfd1f275315f6434f9a2166b3992e4ab8a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-14 23:47:18 -0300

    Update iniciar.sh

--- azure/iniciar.sh
+++ azure/iniciar.sh
@@ -13,8 +13,12 @@ terraform init; terraform plan -out main.tfplan; terraform apply -auto-approve m
 echo ""
 echo "   Aguardando configurações: "
 sleep 10
+echo "   Salvando a chave privada em arquivo (~/environment/labsuser.pem): "
+terraform output -raw private_key_pem > ~/environment/labsuser.pem
+chmod 600 ~/environment/labsuser.pem
+echo "   Verificando conexão com o ambiente: "
 export IP=$(terraform output -raw ip_externo)
-while [ $(ssh -q -oStrictHostKeyChecking=no ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
 sh ajustar.sh
 echo "   Configuração OK."
