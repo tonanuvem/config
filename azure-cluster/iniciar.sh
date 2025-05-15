6252f6f9971cdc935b8745892b73eb43be6cf716
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-15 03:19:03 -0300

    Update iniciar.sh

--- azure-cluster/iniciar.sh
+++ azure-cluster/iniciar.sh
@@ -7,12 +7,32 @@ sleep 10
 echo "   Salvando a chave privada em arquivo (~/environment/labsuser.pem): "
 terraform output -raw private_key_pem > ~/environment/labsuser.pem
 chmod 600 ~/environment/labsuser.pem
-echo "   Verificando conexão com o ambiente: "
-export IP=$(terraform output -raw ip_externo)
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
-echo "   Conectado ao $IP, verificando ajustes: "
+
+MASTER=$(terraform output -json ip_externo | jq .[0] | jq .[] | sed 's/"//g')
+QTD_NODES=$(terraform output -json ip_externo | jq '. | length')
+WORKER_NODES=$(expr $QTD_NODES - 1)
+
+# Aguardando Master
+export IP=$MASTER
+echo "   Aguardando Master com IP = $MASTER: "
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$MASTER "echo CONECTADO1" | grep CONECTADO1 | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao MASTER"
+
+# Aguardando Nodes
+for N in $(seq 1 $WORKER_NODES); do
+    NODE=$(terraform output -json ip_externo | jq .[$N] | jq .[] | sed 's/"//g')
+    echo "   Aguardando Node $N com IP = $NODE: "
+    while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$NODE "echo CONECTADONODE" | grep CONECTADONODE | wc -l) != '1' ]; do { printf .; sleep 1; } done
+    echo "   Conectado ao Node $N"
+done
+
+echo ""
+echo "---"
+echo "   Realizando ajustes (usando o Ansible): "
 sh ajustar.sh
-echo "   Configuração OK."
+
+echo ""
+echo " Nodes (VMs) iniciados e configurados"
 echo ""
 echo "   OBS: Caso alguma das tarefas anteriores tenha obtido FAILED, executar: sh ajustar.sh"
 echo ""
