1e460d851fc196e4b13d6c209b4b15c5da1d4824
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-15 02:03:10 -0300

    Update output.tf

--- azure-cluster/output.tf
+++ azure-cluster/output.tf
@@ -3,24 +3,24 @@ output "resource_group_name" {
 }
 
 output "virtual_network_name" {
-  value = azurerm_virtual_network.test.name
+  value = azurerm_virtual_network.fiaplab_network.name
 }
 
 output "subnet_name" {
-  value = azurerm_subnet.test.name
+  value = azurerm_subnet.fiaplab_subnet.name
 }
 
 output "linux_virtual_machine_names" {
-  value = [for s in azurerm_linux_virtual_machine.test : s.name[*]]
+  value = [for s in azurerm_linux_virtual_machine.clusterfiaplab_vm : s.name[*]]
 }
 
 output "dns_externo" {
-  value = azurerm_public_ip.fiaplab_public_ip.fqdn
+  value = azurerm_public_ip.fiaplab_public_ip[count.index].fqdn
 }
 
 output "ip_externo" {
   #value = azurerm_linux_virtual_machine.fiaplab_vm.public_ip_address
-  value = [for s in azurerm_linux_virtual_machine.fiaplab_vm : s.public_ip_address[*]]
+  value = [for s in azurerm_linux_virtual_machine.clusterfiaplab_vm : s.public_ip_address[*]]
 }
 
 output "private_key_pem" {
