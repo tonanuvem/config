7f90a4640f31e9e858bbdc8a66bfd8492cf98071
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-14 23:10:38 -0300

    Update cloudshell.sh

--- azure/cloudshell.sh
+++ azure/cloudshell.sh
@@ -4,16 +4,16 @@
 # curl -s -H 'Cache-Control: no-cache' -H 'Pragma: no-cache' https://raw.githubusercontent.com/tonanuvem/config/refs/heads/main/azure/cloudshell.sh | bash
 
 # Caminho da pasta
-PASTA_ENV="$HOME/enviroment"
+PASTA_ENV="$HOME/environment"
 PASTA_CONFIG="$PASTA_ENV/config"
 
-# Verifica se a pasta 'enviroment' existe
+# Verifica se a pasta 'environment' existe
 if [ ! -d "$PASTA_ENV" ]; then
     echo "Criando pasta '$PASTA_ENV'..."
     mkdir -p "$PASTA_ENV"
 fi
 
-# Verifica se a pasta 'config' existe dentro de 'enviroment'
+# Verifica se a pasta 'config' existe dentro de 'environment'
 if [ ! -d "$PASTA_CONFIG" ]; then
     echo "Clonando repositório na pasta '$PASTA_ENV'..."
     git clone https://github.com/tonanuvem/config "$PASTA_CONFIG"
