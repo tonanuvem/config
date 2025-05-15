877b02137110d5362a34092ec473a2dcde721696
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-15 03:35:02 -0300

    Update node_conectar.sh

--- azure-cluster/node_conectar.sh
+++ azure-cluster/node_conectar.sh
@@ -8,5 +8,5 @@ fi
 
 NODE=$(terraform output -json ip_externo | jq .[$N] | jq .[] | sed 's/"//g')
 
-echo "Conectando.. IP = $IP"
+echo "Conectando.. IP = $NODE"
 ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$NODE
