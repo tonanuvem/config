7611a482d433cd7e92174b6e15d0ac5e94732f93
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-14 23:48:22 -0300

    Update output.tf

--- azure/output.tf
+++ azure/output.tf
@@ -5,3 +5,8 @@ output "dns_externo" {
 output "ip_externo" {
   value = azurerm_linux_virtual_machine.fiaplab_vm.public_ip_address
 }
+
+output "private_key_pem" {
+  value     = azapi_resource_action.ssh_public_key_gen.output.privateKey
+  sensitive = true
+}
