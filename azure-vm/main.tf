d08fb94532db7b68a8b7aacb3bc193719430cb25
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-16 22:26:52 -0300

    Update main.tf

--- azure-vm/main.tf
+++ azure-vm/main.tf
@@ -23,7 +23,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = "fiap"
+  name     = var.resource_group_name_prefix
 }
 
 # Create virtual network
