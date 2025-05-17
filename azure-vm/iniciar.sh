98e51c3265dc148a485db1ffa42826c072aa1a64
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-17 00:20:50 -0300

    Update iniciar.sh

--- azure-vm/iniciar.sh
+++ azure-vm/iniciar.sh
@@ -1,15 +1,7 @@
-# Verifica se um parâmetro foi passado
-#if [ -n "$1" ]; then
-#  SENHASEGURA="$1"
-#else
-  # Caso não tenha sido passado, solicita ao usuário
-#  echo "Qual SENHASEGURA você deseja usar?"
-#  read -s SENHASEGURA
-#fi
-
 echo ""
 
 terraform init; terraform plan -out main.tfplan; terraform apply -auto-approve main.tfplan 
+
 echo ""
 echo "   Aguardando configurações: "
 sleep 10
@@ -20,7 +12,9 @@ echo "   Verificando conexão com o ambiente: "
 export IP=$(terraform output -raw ip_externo)
 while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
+
 sh ajustar.sh
+
 echo "   Configuração OK."
 echo ""
 echo "   OBS: Caso alguma das tarefas anteriores tenha obtido FAILED, executar: sh ajustar.sh"
