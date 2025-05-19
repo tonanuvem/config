1b8a828a5dd63980b8f5923c55f04f0fb0c7226b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-18 21:52:42 -0300

    Create suspender.sh

---
+++ azure-vm/suspender.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Variáveis
+RESOURCE_GROUP="fiapvm"
+VM_NAME="fiaplab-vm"
+
+# Verifica o estado atual da VM
+VM_STATE=$(az vm get-instance-view \
+  --resource-group $RESOURCE_GROUP \
+  --name $VM_NAME \
+  --query "instanceView.statuses[?starts_with(code, 'PowerState/')].code" \
+  --output tsv)
+
+echo ""
+echo "Estado atual da VM '$VM_NAME': $VM_STATE"
+echo ""
+if [[ "$VM_STATE" == "PowerState/deallocated" || "$VM_STATE" == "PowerState/stopped" ]]; then
+    echo "A VM está desligada. Vamos INICIAR!"
+    echo ""
+    az vm start --resource-group $RESOURCE_GROUP --name $VM_NAME
+    echo "VM iniciada."
+    echo ""
+    echo "Atualizando as informações da VM:"
+    terraform apply --auto-approve > /dev/null
+    export IP=$(terraform output -raw $NODE)
+    echo "   IP = $IP.."
+    echo ""
+else
+    echo "Estado não suportado: $VM_STATE"
+fi
+echo ""
