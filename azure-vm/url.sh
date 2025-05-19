24218ba9d9322cd249fc4a706b75fe33daf6f4f7
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-18 22:00:58 -0300

    Update url.sh

--- azure-vm/url.sh
+++ azure-vm/url.sh
@@ -1,3 +1,6 @@
+echo "   Verificando Configuração."
+export IP=$(terraform output -raw ip_externo)
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Configuração OK."
 echo ""
 echo "   ACESSAR: http://$IP:8099              ( senha = fiap )"
