44bb71324e0f59d323e3839e463743d8abf83220
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-06-13 14:06:32 -0300

    Update node_conectar.sh

--- azure-cluster/node_conectar.sh
+++ azure-cluster/node_conectar.sh
@@ -2,11 +2,11 @@ if [ -n "$1" ]; then
   NODENUM="$1"
 else
   # Caso não tenha sido passado, solicita ao usuário
-  echo "Em qual NODE você deseja conectar? Digitar: 1 ou 2 ou 3"
+  echo "Em qual NODE você deseja conectar? Digitar: 1 ou 2"
   read NODENUM
 fi
 
-NODE=$(terraform output -json ip_externo | jq .[$NODENUM] | jq .[] | sed 's/"//g')
+NODE=$(terraform output -json ip_externo | jq -r '.[NODENUM]')
 
 echo "Conectando.. IP = $NODE"
 ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$NODE
