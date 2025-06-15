6f59c86ad97fe991ac57ab79034096b842aa46a5
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-06-14 22:32:04 -0300

    Update k8s_create_cluster.sh

--- azure-cluster/k8s_create_cluster.sh
+++ azure-cluster/k8s_create_cluster.sh
@@ -1,10 +1,11 @@
 # conectar no master e configurar
 
-export DIRETORIO='/home/ec2-user'
+export DIRETORIO='/home/ubuntu'
 # ~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") ) { print } }'
 
-MASTER=$(terraform output -json ip_externo | jq .[] | jq .[0] | sed 's/"//g')
-QTD_NODES=$(terraform output -json ip_externo | jq '.[] | length')
+# Corrigido: extraindo IPs da nova estrutura do output
+MASTER=$(terraform output -json ip_externo | jq -r '.[0]')
+QTD_NODES=$(terraform output -json ip_externo | jq '. | length')
 WORKER_NODES=$(expr $QTD_NODES - 1)
 
 # Criando arquivos vazios para receber os comandos
@@ -23,7 +24,7 @@ echo "sudo kubeadm config images pull" >> master.sh
 echo "sudo kubeadm init --control-plane-endpoint \$(curl checkip.amazonaws.com):6443" >> master.sh
 
 ### INICIANDO O MASTER via SSH
-ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < master.sh
+ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$MASTER 'bash -s' < master.sh
 
 #	Configurar o cliente kubectl:
 echo "mkdir -p $DIRETORIO/.kube" > master.sh
@@ -31,9 +32,9 @@ echo "sudo cp -i /etc/kubernetes/admin.conf $DIRETORIO/.kube/config" >> master.s
 echo "sudo chown $(id -u):$(id -g) $DIRETORIO/.kube/config" >> master.sh
 
 ### CONFIGURANDO O MASTER via SSH
-ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < master.sh
+ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$MASTER 'bash -s' < master.sh
 # Get Token
-TOKEN=$(ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'sudo kubeadm token create --print-join-command')
+TOKEN=$(ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$MASTER 'sudo kubeadm token create --print-join-command')
 printf "\n\n"
 echo $TOKEN
 printf "\n\n"
@@ -41,7 +42,7 @@ echo "   TOKEN ACIMA : KUBEADM JOIN"
 printf "\n\n"
 echo "Master do Cluster foi inicializado. Agora vamos configurar a rede do cluster."
 # Configurar a rede do cluster:
-ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'bash -s' < k8s_config_network_weave.sh
+ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$MASTER 'bash -s' < k8s_config_network_weave.sh
 
 ### CONFIGURANDO OS NODES utilizando o KUBEADM JOIN:
 
@@ -55,19 +56,19 @@ echo "sudo $TOKEN" > workers.sh
 
 for N in $(seq 1 $WORKER_NODES); do
     printf "\n\n"
-    NODE=$(terraform output -json ip_externo | jq .[] | jq .[$N] | sed 's/"//g')
+    NODE=$(terraform output -json ip_externo | jq -r ".[$N]")
     echo "   CONFIGURANDO NODE $N ($NODE): KUBEADM JOIN"
-    ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$NODE 'bash -s' < workers.sh
+    ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$NODE 'bash -s' < workers.sh
 done
 
 echo "   VERIFICANDO NODES NO MASTER :"
 printf "\n\n"
-ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'kubectl get nodes'
+ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$MASTER 'kubectl get nodes'
 
 ### CONFIGURANDO OS VOLUMES 
 #sh k8s_criar_volume_portworx.sh
 
 printf "\n\n"
 echo "   CONFIGURAÇÕES REALIZADAS. FIM."
-ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ec2-user@$MASTER 'kubectl get nodes'
+ssh -oStrictHostKeyChecking=no -i ~/environment/labsuser.pem ubuntu@$MASTER 'kubectl get nodes'
 printf "\n\n"
