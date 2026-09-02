743d90ac4731aa1be32789d43958ca14b54bf447
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-09-02 09:33:04 -0300

    Create storage.tf for Terraform S3 backend
    Add Terraform configuration for S3 backend storage

---
+++ ubuntu-vm/storage.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    # bucket       = "BUCKET_NAME" # O bucket será injetado dinamicamente no init com comando : terraform init -backend-config="bucket=tfstate-cloudshell-$(aws sts get-caller-identity --query Account --output text)"
+    key            = "ubuntu-vm/terraform.tfstate" # Apenas altere o caminho para cada subpasta
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
