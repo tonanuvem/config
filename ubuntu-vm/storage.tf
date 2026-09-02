477773ff3ba92eba9b9dfca885b51d906b198942
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-09-02 19:00:12 -0300

    Update S3 bucket configuration in storage.tf

--- ubuntu-vm/storage.tf
+++ ubuntu-vm/storage.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "s3" {
     # bucket       = "BUCKET_NAME" # O bucket será injetado dinamicamente no init com comando : terraform init -backend-config="bucket=tfstate-cloudshell-$(aws sts get-caller-identity --query Account --output text)"
+    bucket         = "tfstate-cloudshell"
     key            = "ubuntu-vm/terraform.tfstate" # Apenas altere o caminho para cada subpasta
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
-    use_lockfile = true
+    use_lockfile   = true
     encrypt        = true
   }
 }
