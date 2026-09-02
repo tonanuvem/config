58dac9001062ccdd7d1206e8c97cecd544ded114
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-09-02 11:51:35 -0300

    Refactor ansible-playbook commands in ajustar.sh
    Refactored ansible-playbook commands to use a variable for the playbook command.

--- ubuntu-vm/ajustar.sh
+++ ubuntu-vm/ajustar.sh
@@ -147,13 +147,40 @@ echo "        AJUSTANDO VIA ANSIBLE"
 echo "============================================================"
 echo ""
 
-ansible-playbook ~/environment/config/ansible/ansible_hostname.yml --inventory hosts -u ubuntu --key-file ~/environment/labsuser.pem # --extra-vars "checar_Ambiente=sim"
-ansible-playbook ~/environment/config/ansible/ansible_desligamento.yml --inventory hosts -u ubuntu --key-file ~/environment/labsuser.pem
-ansible-playbook ~/environment/config/ansible/ansible_utils.yml --inventory hosts -u ubuntu --key-file ~/environment/labsuser.pem
-ansible-playbook ~/environment/config/ansible/ansible_docker.yml --inventory hosts -u ubuntu --key-file ~/environment/labsuser.pem
-ansible-playbook ~/environment/config/ansible/ansible_k8s.yml --inventory hosts -u ubuntu --key-file ~/environment/labsuser.pem
-ansible-playbook ~/environment/config/ansible/ansible_dev_java.yml --inventory hosts -u ubuntu --key-file ~/environment/labsuser.pem
-ansible-playbook ~/environment/config/ansible/ansible_code_server_ubuntu.yml --inventory hosts -u ubuntu --key-file ~/environment/labsuser.pem
+"$ANSIBLE_PLAYBOOK" ~/environment/config/ansible/ansible_hostname.yml \
+    --inventory inv.hosts \
+    -u ubuntu \
+    --key-file ~/environment/labsuser.pem
+
+"$ANSIBLE_PLAYBOOK" ~/environment/config/ansible/ansible_desligamento.yml \
+    --inventory inv.hosts \
+    -u ubuntu \
+    --key-file ~/environment/labsuser.pem
+
+"$ANSIBLE_PLAYBOOK" ~/environment/config/ansible/ansible_utils.yml \
+    --inventory inv.hosts \
+    -u ubuntu \
+    --key-file ~/environment/labsuser.pem
+
+"$ANSIBLE_PLAYBOOK" ~/environment/config/ansible/ansible_docker.yml \
+    --inventory inv.hosts \
+    -u ubuntu \
+    --key-file ~/environment/labsuser.pem
+
+"$ANSIBLE_PLAYBOOK" ~/environment/config/ansible/ansible_k8s.yml \
+    --inventory inv.hosts \
+    -u ubuntu \
+    --key-file ~/environment/labsuser.pem
+
+"$ANSIBLE_PLAYBOOK" ~/environment/config/ansible/ansible_dev_java.yml \
+    --inventory inv.hosts \
+    -u ubuntu \
+    --key-file ~/environment/labsuser.pem
+
+"$ANSIBLE_PLAYBOOK" ~/environment/config/ansible/ansible_code_server_ubuntu.yml \
+    --inventory inv.hosts \
+    -u ubuntu \
+    --key-file ~/environment/labsuser.pem
 
 
 echo ""
