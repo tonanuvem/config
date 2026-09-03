3350d0873f34528c6a2dbee03ac96f901192a613
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-09-03 12:28:24 -0300

    Update main.tf

--- ubuntu-vm/main.tf
+++ ubuntu-vm/main.tf
@@ -26,6 +26,7 @@ resource "aws_subnet" "default" {
   vpc_id                  = aws_vpc.default.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 }
 
 # Cria um "security group" para o EC2 visando permitir o acesso Web
